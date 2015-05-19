9a16302561a093c08f201b127aa9fcf14b8f5b67
emcenrue <edward.mcenrue@gmail.com>
2015-05-18 23:17:47 -0400

    fixing minor bugs

--- Python/CodeForces/CF301_Ice_Cave.py
+++ Python/CodeForces/CF301_Ice_Cave.py
@@ -1,13 +1,28 @@
 __author__ = 'emcenrue'
 import queue
+#TODO replace with deque
+
 
 class Position(object):
     def __init__(self, row, column):
         self.row = row
         self.column = column
 
+    def __eq__(self, other):
+        if(self.row == other.row and self.column == other.column):
+            return True
+        else:
+            return False
 
-def createState(stateMatrix, position):
+n, m = list(map(int, input().split()))
+startMatrix = [list(input()) for x in range(n)] #startMatrix[row][column]
+startRow, startColumn = list(map(int, input().split()))
+startPos = Position(startRow-1, startColumn-1)
+endRow, endColumn = list(map(int, input().split()))
+endPos = Position(endRow-1, endColumn-1)
+
+
+def create_state(stateMatrix, position):
     newMatrix = list()
     newMatrix = [stateMatrix[row] for row in range(len(stateMatrix))]
     newMatrix[position.row][position.column] = 'X'
@@ -22,9 +37,9 @@ class State(object):
     def __hash__(self):
         hash = 0
         mult = 1
-        for row in range(len(self.matrix)):
-            for col in range(len(self.matrix[0])):
-                if(self.matrix[row][col] == 'X'):
+        for row in range(len(self.curMatrix)):
+            for col in range(len(self.curMatrix[0])):
+                if(self.curMatrix[row][col] == 'X'):
                     hash += mult * 1
                 mult*=2
         return hash
@@ -38,33 +53,35 @@ class State(object):
 
 
     #adjacentStates
-    def getAdjacentStates(self):
+    def get_adjacent_states(self):
         #calculate a new matrix from curMatrix based on horizontal and vertical position movements
         #curMatrix & curPosition
         adjStates = list()
-        if(self.curPosition.column > 0
-           and not(self.curMatrix[self.curPosition.row][self.curPosition.column - 1])):
+        if(self.curPosition.column > 0):
             # Subtract one from the column of the current position
             leftPos = Position(self.curPosition.row, self.curPosition.column - 1)
-            adjStates.append(createState(self.curMatrix, leftPos))
+            if(self.curMatrix[leftPos.row][leftPos.column] == '.' or leftPos.__eq__(endPos)):
+                adjStates.append(create_state(self.curMatrix, leftPos))
 
-        if(self.curPosition.column < len(self.curMatrix[0]) - 1
-           and not(self.curMatrix[self.curPosition.row][self.curPosition.column + 1] == 'X')):
+        if(self.curPosition.column < len(self.curMatrix[0]) - 1):
             # Add one to the column of the current position
             rightPos = Position(self.curPosition.row, self.curPosition.column + 1)
-            adjStates.append(createState(self.curMatrix, rightPos))
+            if(self.curMatrix[rightPos.row][rightPos.column] == '.' or rightPos.__eq__(endPos)):
+                adjStates.append(create_state(self.curMatrix, rightPos))
 
-        if(self.curPosition.row > 0
-           and not(self.curMatrix[self.curPosition.row - 1][self.curPosition.column] == 'X')):
+        if(self.curPosition.row > 0):
             # Subtract one from the current position's row
             downPos = Position(self.curPosition.row-1, self.curPosition.column)
-            adjStates.append(createState(self.curMatrix, downPos))
+            if(self.curMatrix[downPos.row][downPos.column] == '.' or downPos.__eq__(endPos)):
+                adjStates.append(create_state(self.curMatrix, downPos))
 
         if(self.curPosition.row < len(self.curMatrix) - 1
            and not(self.curMatrix[self.curPosition.row + 1][self.curPosition.column] == 'X')):
             # Add one to the row of the current position
             upPos = Position(self.curPosition.row + 1, self.curPosition.column)
-            adjStates.append(createState(self.curMatrix, upPos))
+
+            if(self.curMatrix[upPos.row][upPos.column] == '.' or upPos.__eq__(endPos)):
+                adjStates.append(create_state(self.curMatrix, upPos))
         #print()
         return adjStates
 
@@ -84,25 +101,23 @@ class State(object):
 
 
 
-n, m = list(map(int, input().split()))
-startMatrix = [list(input()) for x in range(n)] #startMatrix[row][column]
-startRow, startColumn = list(map(int, input().split()))
-startPos = Position(startRow-1, startColumn-1)
-endRow, endColumn = list(map(int, input().split()))
-endPos = Position(endRow-1, endColumn-1)
 
-initialState = createState(startMatrix, startPos)
 
-testAdj = initialState.getAdjacentStates()
+initialState = create_state(startMatrix, startPos)
+
+testAdj = initialState.get_adjacent_states()
 for adj in testAdj:
     adj.__repr__()
     print()
 
-def isFinalState(curState):
+
+
+def is_final_state(curState):
     if(curState.curPosition.row == endPos.row and curState.curPosition.column == endPos.column):
         return True
     else:
         return False
+
 
 
 bfsCompleteFlag = 0
@@ -110,6 +125,7 @@ def matrixBFS(initialState):
     # initialize datastructures
     knownSet = dict()
     toBeVisited = queue()
+
 
     # give the algorithm initial parameters
     knownSet[initialState.__hash__()] = 1
@@ -120,22 +136,29 @@ def matrixBFS(initialState):
         curState = toBeVisited.get()
         knownSet[curState.initialState.__hash__()] = 1
 
+        for i, row in enumerate(curState.curMatrix):
+            for j, col in enumerate(curState.curMatrix[i]):
+                print(curState.curMatrix[i][j], end='')
+            print()
+        print("above this shit")
+
         #check to see if it's the final state, if so flag, break, and print
-        if(isFinalState(curState)):
+        if(is_final_state(curState)):
             bfsCompleteFlag = 1
             break
 
         #get the curState's neighbors, and queue them up if they haven't been visited yet
-        for adj in curState.getAdjacentStates():
+        for adj in curState.get_adjacent_states():
             if not(knownSet[adj.__hash__()] == 1):
                 toBeVisited.put(adj)
 
 
-
+matrixBFS(initialState)
 if(bfsCompleteFlag):
     print("YES")
 else:
     print("NO")
+
 
 
 
